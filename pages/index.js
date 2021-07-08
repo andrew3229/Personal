bce27210952eda013a2caa3fb1941f7afa27337c
andrew3229 <dacv3229@gmail.com>
2021-07-08 12:01:03 -0500

    todos los archivos :v

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+/* ./pages/index.js               */
+import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Navbar />
+      <div>Hello World</div>
+    </div>
+  );
+}
---
+++ pages/prueba.js
@@ -0,0 +1,10 @@
+import { Navbar } from '../components/Navbar';
+
+const Prueba = () => (
+  <>
+    <Navbar/>
+    <h1>Prueba page</h1>
+  </>
+)
+
+export default Prueba
